9b76e139725873b0b8e89351d99f2348824c4ee5
duckgocloud <linxiaoting100@gmail.com>
2023-02-19 23:20:29 +0100

    practice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Portfolio from './components/project/Portfolio';
 import HeaderSocials from './components/header/HealderSocials';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
     <div className="App">
       <HeaderSocials />
       <Portfolio />
+      <Contact />
     </div>
   );
 }
