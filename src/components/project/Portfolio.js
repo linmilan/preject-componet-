df89e8d5bd622fa63168dc4e90468ad0a1e28a89
duckgocloud <linxiaoting100@gmail.com>
2023-02-19 23:06:56 +0100

    practice

--- src/components/project/Portfolio.js
+++ src/components/project/Portfolio.js
@@ -135,8 +135,8 @@ const Portfolio =()=>{
                                 </div>
                                 {/* <h3>{title}</h3> */}
                                 <div className='portfolio_item-cta'> 
-                                    <a href={github} className='btn' target='_blank'>Github</a>
-                                    <a href={demo} className='btn btn-primary' target='_blank'>Demo</a>
+                                    <a href={github} className='btn' target='_blank' rel='noreferrer'>Github</a>
+                                    <a href={demo} className='btn btn-primary' target='_blank' rel='noreferrer'>Demo</a>
                                 </div> 
                             </article>
                         )
