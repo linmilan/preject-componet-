0325962e3b50fedf11999394e7f5c1f8c2478b32
duckgocloud <linxiaoting100@gmail.com>
2023-02-22 15:38:34 +0100

    practive

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Portfolio from './components/project/Portfolio';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <Nav />
       <Portfolio />
       <Contact />
       <Footer />
