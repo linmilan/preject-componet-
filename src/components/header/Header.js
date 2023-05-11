1161992d6d4bf17d8826cd456a22c5d82831fbaa
duckgocloud <linxiaoting100@gmail.com>
2023-05-11 18:58:39 +0200

    remove img

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './header.css'
 import CTA from "./CTA";
-import ME from '../../assets/img/me.png'
+
 import HeaderSocials from "./HealderSocials";
 
 const Header = () => {
@@ -15,7 +15,7 @@ const Header = () => {
                 <HeaderSocials />
 
                 <div className="me">
-                    <img src={ME} alt='me' />
+                    {/* <img src={ME} alt='me' /> */}
                 </div>
 
                 <a href="#contact" className="scroll_down">Scroll Down</a>
